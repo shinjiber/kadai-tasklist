e3946ae8dbb464f58aac2e19d905b82f600f30e7
jienshinji.yamaguchi <shinjiber7@gmail.com>
2018-12-25 11:57:22 +0000

    add new column status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,13 @@
 class TasksController < ApplicationController
   
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
   
   
   def show
-    @task = Task.find(params[:id])
   end
   
   
@@ -31,12 +32,10 @@ class TasksController < ApplicationController
   
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   
   def update
-    @task = Task.find(params[:id])
     
     if @task.update(task_params)
       flash[:success] = 'Task was updated successfully'
@@ -49,7 +48,6 @@ class TasksController < ApplicationController
   
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = "Task was deleted successfully"
@@ -58,8 +56,12 @@ class TasksController < ApplicationController
 
   private
   
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 
 end
--- app/models/kadaione.rb
+++
@@ -1,2 +0,0 @@
-class Kadaione < ApplicationRecord
-end
