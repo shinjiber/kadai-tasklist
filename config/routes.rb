7eed9c44a5a1363a083944c5cf08db4692dc8fb3
jienshinji.yamaguchi <shinjiber7@gmail.com>
2019-01-08 08:34:31 +0000

    modify gridsystem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  root to: 'toppages#index'
+ 
+ 
+ get 'login', to: 'sessions#new' 
+ post 'login', to: 'sessions#create'
+ get 'logout', to: 'sessions#destroy'
   
-  root to: 'tasks#index'
+  get 'signup', to: 'users#new'
+  resources :users, only:[:index, :show, :new, :create]
   
   resources :tasks
   
