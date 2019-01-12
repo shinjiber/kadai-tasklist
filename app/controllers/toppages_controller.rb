d85ae6d6b9c4e50cdeecade15a4c3f422cbe4b23
jienshinji.yamaguchi <shinjiber7@gmail.com>
2019-01-12 03:04:25 +0000

    for heroku after modify

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,8 +1,20 @@
 class ToppagesController < ApplicationController
+  before_action :correct_user
+  
   def index
     if logged_in?
       @task = current_user.tasks.build
       @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+    end
+  end
+  
+  
+  private
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
     end
   end
 end
